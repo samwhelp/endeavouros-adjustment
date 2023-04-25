81aaf3ca903cd1a5624e01f47bc09f26481fc49d
samwhelp <samwhelp@users.noreply.github.com>
2023-04-26 00:05:58 +0800

    fix

--- project/endeavouros-adjustment-system/endeavouros-adjustment-iso-profile/recipe/start/build-from-prefile/build.sh
+++ project/endeavouros-adjustment-system/endeavouros-adjustment-iso-profile/recipe/start/build-from-prefile/build.sh
@@ -127,7 +127,9 @@ mod_iso_make () {
 	util_error_echo
 
 	#sudo mkarchiso -v profile
-	sudo mkarchiso -w tmp/work -o tmp/out -v profile
+	#sudo mkarchiso -w tmp/work -o tmp/out -v profile
+
+	sudo ./profile/mkarchiso -w tmp/work -o tmp/out -v profile
 
 
 }
